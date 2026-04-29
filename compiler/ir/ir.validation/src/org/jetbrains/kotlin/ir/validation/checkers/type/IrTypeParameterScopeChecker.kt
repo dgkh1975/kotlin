a8a5ae07c55a11eb51a3f5e3813895450830c263
Miłosz Piekutowski <milosz.piekutowski@jetbrains.com>
2026-04-29 19:23:39 +0200

    [IR] Exclude `typeOf` from type parameter visibility checks
    This intrinsic is allowed to use out-of-scope type parameters, so it
    shouldn't be reported by `IrTypeParameterScopeChecker`
    
    KT-69305

--- compiler/ir/ir.validation/src/org/jetbrains/kotlin/ir/validation/checkers/type/IrTypeParameterScopeChecker.kt
+++ compiler/ir/ir.validation/src/org/jetbrains/kotlin/ir/validation/checkers/type/IrTypeParameterScopeChecker.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
 import org.jetbrains.kotlin.ir.types.IrSimpleType
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.util.isTypeOfIntrinsicCall
 import org.jetbrains.kotlin.ir.util.render
 import org.jetbrains.kotlin.ir.validation.checkers.IrTypeChecker
 import org.jetbrains.kotlin.ir.validation.checkers.context.CheckerContext
@@ -37,6 +38,7 @@ object IrTypeParameterScopeChecker : IrTypeChecker {
         element: IrElement,
         typeParameterSymbol: IrTypeParameterSymbol,
     ) {
+        if (element.isTypeOfIntrinsicCall()) return
         if (!context.typeParameterScopeStack.isVisibleInCurrentScope(typeParameterSymbol)) {
             context.error(
                 element,
