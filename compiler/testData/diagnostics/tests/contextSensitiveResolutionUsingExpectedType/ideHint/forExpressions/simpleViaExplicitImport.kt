ewok-pulse/saber-e2e-kotlin

FIR: Provide CSR IDE hint for simple names resolved through imports
In IDE mode, a simple name like `X` that resolves through `import A.*`
or `import A.X` now also receives the `ContextSensitiveResolutionMightBeUsed`
non-fatal diagnostic when context-sensitive resolution against the
surrounding expected type would resolve the same name to the same symbol.
This lets IDE shorteners / unused-import inspections detect that the
import is removable.

The fix mirrors the qualified-form mechanism added in 69dcb024734a
(KT-84717) and extended for type operators in 6ca894e8ad0b (KT-84719):

- `prepareContextSensitiveAlternativeIfNeeded` now also dispatches when
  `original.explicitReceiver == null`. For an already-resolved simple
  name whose `resolvedSymbolOrigin` is `StarImport` or `ExplicitImport`,
  the new `prepareCSRHintForSimpleNameResolvedViaImport` either runs
  CSR immediately (`WithExpectedType`) or stores a fresh simple-name
  copy as `contextSensitiveAlternative` (`ContextDependent`) so the
  call completer can run CSR + symbol comparison via the existing
  `ConeContextSensitiveAlternativeForQualifierAtom` machinery. Both
  `FirPropertyAccessExpression` (callable refs / enum entries) and
  `FirResolvedQualifier` (objects, sealed sub-objects) are handled.

- `prepareCSRHintForTypeOperatorIfNeeded` now also handles single-
  qualifier types when `resolvedSymbolOrigin` is `StarImport` or
  `ExplicitImport`; the "does the simple name resolve normally" guard
  is skipped because it does, by construction.

- `ArgumentCheckingProcessor.resolveArgumentExpression` routes simple-
  name argument atoms with a `contextSensitiveAlternative` set to
  `preprocessQualifierWithContextSensitiveAlternative` so the call
  completer treats them the same as qualified hints.

^KT-84718 Fixed

compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/ideHint/forExpressions/simpleViaExplicitImport.kt
@@ -16,8 +16,8 @@ import test.A.Y
 fun expectsA(x: A) {}
 
 fun main() {
-    expectsA(X)
-    val a: A = Y
+    expectsA(<!DEBUG_INFO_CSR_MIGHT_BE_USED!>X<!>)
+    val a: A = <!DEBUG_INFO_CSR_MIGHT_BE_USED!>Y<!>
 }
 
 /* GENERATED_FIR_TAGS: enumDeclaration, enumEntry, functionDeclaration, localProperty, propertyDeclaration */

compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/ideHint/forExpressions/simpleViaStarImport.kt
@@ -15,8 +15,8 @@ import test.A.*
 fun expectsA(x: A) {}
 
 fun main() {
-    expectsA(X)
-    val a: A = Y
+    expectsA(<!DEBUG_INFO_CSR_MIGHT_BE_USED!>X<!>)
+    val a: A = <!DEBUG_INFO_CSR_MIGHT_BE_USED!>Y<!>
 }
 
 /* GENERATED_FIR_TAGS: enumDeclaration, enumEntry, functionDeclaration, localProperty, propertyDeclaration */

compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/ideHint/forTypes/simpleViaStarImport.kt
@@ -14,11 +14,11 @@ import test.A
 import test.A.*
 
 fun foo(a: A) {
-    if (a is X) {
+    if (a is <!DEBUG_INFO_CSR_MIGHT_BE_USED!>X<!>) {
         "".hashCode()
     }
 
-    if (a !is Y) {
+    if (a !is <!DEBUG_INFO_CSR_MIGHT_BE_USED!>Y<!>) {
         "".hashCode()
     }
 }

dgkh1975/kotlin

compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/ideHint/forExpressions/simpleViaExplicitImport.kt
@@ -16,8 +16,8 @@ import test.A.Y
 fun expectsA(x: A) {}
 
 fun main() {
-    expectsA(X)
-    val a: A = Y
+    expectsA(<!DEBUG_INFO_CSR_MIGHT_BE_USED_INSTEAD_OF_IMPORT!>X<!>)
+    val a: A = <!DEBUG_INFO_CSR_MIGHT_BE_USED_INSTEAD_OF_IMPORT!>Y<!>
 }
 
 /* GENERATED_FIR_TAGS: enumDeclaration, enumEntry, functionDeclaration, localProperty, propertyDeclaration */

compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/ideHint/forExpressions/simpleViaStarImport.kt
@@ -15,8 +15,8 @@ import test.A.*
 fun expectsA(x: A) {}
 
 fun main() {
-    expectsA(X)
-    val a: A = Y
+    expectsA(<!DEBUG_INFO_CSR_MIGHT_BE_USED_INSTEAD_OF_IMPORT!>X<!>)
+    val a: A = <!DEBUG_INFO_CSR_MIGHT_BE_USED_INSTEAD_OF_IMPORT!>Y<!>
 }
 
 /* GENERATED_FIR_TAGS: enumDeclaration, enumEntry, functionDeclaration, localProperty, propertyDeclaration */

compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/ideHint/forTypes/simpleViaStarImport.kt
@@ -14,11 +14,11 @@ import test.A
 import test.A.*
 
 fun foo(a: A) {
-    if (a is X) {
+    if (a is <!DEBUG_INFO_CSR_MIGHT_BE_USED_INSTEAD_OF_IMPORT!>X<!>) {
         "".hashCode()
     }
 
-    if (a !is Y) {
+    if (a !is <!DEBUG_INFO_CSR_MIGHT_BE_USED_INSTEAD_OF_IMPORT!>Y<!>) {
         "".hashCode()
     }
 }